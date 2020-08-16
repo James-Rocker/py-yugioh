a6d0b3bb03a9d6c8dbb2d8ef56db3d4d612f0c59
TheRockerfly <james.rocker@hotmail.co.uk>
2020-08-16 10:41:38 +0100

    improved the structure of the MetaDecks class and added get_all_most_used_cards and get_main_deck methods

--- soure/get_meta.py
+++ soure/get_meta.py
@@ -1,25 +1,44 @@
+import random
+
 from bs4 import BeautifulSoup
 import requests
 
 
 class MetaDecks:
     def __init__(self):
-        self.url = requests.get('https://yugiohtopdecks.com/')
-        self.soup = BeautifulSoup(self.url.text, features="html.parser")
+        self.url = 'https://yugiohtopdecks.com/'
+        self.meta_dict = {}
 
     def __get_meta_tags(self):
-        for link in self.soup.findAll('td'):
+        request_get = requests.get(self.url)
+        soup = BeautifulSoup(request_get.text, features="html.parser")
+        for link in soup.findAll('td'):
             yield link
 
     def get_meta_listing(self):
-        meta_dict = {}
         tags = self.__get_meta_tags()
         counter = 1
         for items in tags:
             for sub_link in items.findAll('a'):
                 for tag in sub_link.findAll('b'):
                     deck = tag.string.replace("\n", '')
-                    meta_dict[counter] = deck
+                    self.meta_dict[counter] = deck
                     counter += 1
-        return meta_dict
+        return self.meta_dict
 
+    def get_all_most_used_cards(self, deck_type=''):
+        """
+        if no deck_type is passed then a random type will be selected
+        :param deck_type:
+        :return:
+        """
+        if deck_type == '':
+            deck_type = random.choice(list(self.get_meta_listing().values())).replace(' ', '+')
+        most_used_cards_url = f"{self.url}most_used_cards_in_deck/" + deck_type
+        get_request = requests.get(most_used_cards_url)
+        soup = BeautifulSoup(get_request.text, features="html.parser")
+        return soup
+
+    def get_main_deck(self, deck_type=''):
+        page = self.get_all_most_used_cards(deck_type)
+        return page
